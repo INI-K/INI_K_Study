80fa5636665299477ec2cf4e3daa5d3e1c9880fa
INI-K <annette57@naver.com>
2024-01-17 14:56:58 +0900

    minMax

---
+++ Edu_Kotiln_Variable/src/main/kotlin/Main.kt
@@ -0,0 +1,19 @@
+fun main(args: Array<String>) {
+//    val number = 10
+//    var language = "Korean"
+//    val secondNumber: Int = 20
+//    language = "Engilsh"
+//
+//    println("number: $number")
+//    println("language: $language")
+//    println("secondNumber: $secondNumber")
+
+    var num: Double = 0.1
+
+    for(x in 0..999){
+        num += 0.1
+    }
+
+    println(num)
+
+}

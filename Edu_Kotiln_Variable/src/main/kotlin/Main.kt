e2f190876908d1cc6a6633f76af7f048e4cd4466
INI-K <annette57@naver.com>
2024-01-17 15:21:56 +0900

    NullTest

--- Edu_Kotiln_Variable/src/main/kotlin/Main.kt
+++ Edu_Kotiln_Variable/src/main/kotlin/Main.kt
@@ -1,3 +1,4 @@
+
 fun main(args: Array<String>) {
 //    val number = 10
 //    var language = "Korean"
@@ -7,18 +8,29 @@ fun main(args: Array<String>) {
 //    println("number: $number")
 //    println("language: $language")
 //    println("secondNumber: $secondNumber")
+//-----
+//    var num: Double = 0.1
+//
+//    for(x in 0..999){
+//        num += 0.1
+//    }
+//
+//    println(num)
+//    println("Byte min: " + Byte.MIN_VALUE + " max: " + Byte.MAX_VALUE)
+//    println("Short min: " + Short.MIN_VALUE + " max: " + Short.MAX_VALUE)
+//    println("Int min: " + Int.MIN_VALUE + " max: " + Int.MAX_VALUE)
+//    println("Long min: " + Long.MIN_VALUE + " max: " + Long.MAX_VALUE)
+//    println("Float min: " + Float.MIN_VALUE + " max: " + Long.MAX_VALUE)
+//    println("Double min : " + Double.MIN_VALUE + " max: " + Double.MAX_VALUE)
+//---
+    var str1: String? = "Hello Kotlin"
+    str1 = null
+    val len = if(str1 != null) str1.length else -1
+    println("str1: $str1 length: ${len}")
 
-    var num: Double = 0.1
 
-    for(x in 0..999){
-        num += 0.1
-    }
+    var str2: String? = "Hello Kotlin"
+    str2 = null
+    println("str2: $str2 length: ${str2?.length ?: -1}")
 
-    println(num)
-    println("Byte min: " + Byte.MIN_VALUE + " max: " + Byte.MAX_VALUE)
-    println("Short min: " + Short.MIN_VALUE + " max: " + Short.MAX_VALUE)
-    println("Int min: " + Int.MIN_VALUE + " max: " + Int.MAX_VALUE)
-    println("Long min: " + Long.MIN_VALUE + " max: " + Long.MAX_VALUE)
-    println("Float min: " + Float.MIN_VALUE + " max: " + Long.MAX_VALUE)
-    println("Double min : " + Double.MIN_VALUE + " max: " + Double.MAX_VALUE)
 }

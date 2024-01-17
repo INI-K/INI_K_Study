a81c0e6201a62117e49f0bac711473c16bc39067
INI-K <annette57@naver.com>
2024-01-17 15:06:26 +0900

    MinMax

--- Edu_Kotiln_Variable/src/main/kotlin/Main.kt
+++ Edu_Kotiln_Variable/src/main/kotlin/Main.kt
@@ -15,5 +15,10 @@ fun main(args: Array<String>) {
     }
 
     println(num)
-
+    println("Byte min: " + Byte.MIN_VALUE + " max: " + Byte.MAX_VALUE)
+    println("Short min: " + Short.MIN_VALUE + " max: " + Short.MAX_VALUE)
+    println("Int min: " + Int.MIN_VALUE + " max: " + Int.MAX_VALUE)
+    println("Long min: " + Long.MIN_VALUE + " max: " + Long.MAX_VALUE)
+    println("Float min: " + Float.MIN_VALUE + " max: " + Long.MAX_VALUE)
+    println("Double min : " + Double.MIN_VALUE + " max: " + Double.MAX_VALUE)
 }
